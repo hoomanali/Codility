577997a856bd100729f411d22792ca4c658e6fb8
Ali Hooman <me@alihooman.com>
2020-03-29 13:43:53 -0600

    Counting Elements: MaxCounters

--- CountingElements_MaxCounters.java
+++ CountingElements_MaxCounters.java
@@ -54,3 +54,82 @@
  * N and M are integers within the range [1..100,000];
  * each element of array A is an integer within the range [1..N + 1].
  */
+/*
+ * Score: 100%
+ */
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int N, int[] A) {
+        /*
+         * Create an array of N counters, each index is the counter number and
+         * the value at that index is the counter's current count.
+         */
+        int[] counters = new int[N];
+
+        /*
+         * Java compilers will initialize int values to 0, but just in case...
+         */
+        Arrays.fill(counters, 0);
+
+        /*
+         * The minimum starting count for each counter. Initially 0, but this is
+         * increased to maxCount whenever a maxCounters operation occurs.
+         * maxCount is the current maximum counter value.
+         */
+        int startCount = 0;
+        int maxCount = 0;
+        
+        /*
+         * Loop through every operation in array A, if operation < N, then increment
+         * counter (operation - 1).
+         *
+         * If the operation is greater than the range of counters, set the starting
+         * count to the current maximum.
+         *
+         * If the counter being incremented hasn't been increased to the maximum
+         * count, increase it then increment it.
+         *
+         * If the operation is within the range of counters, increment the counter.
+         */
+        for(int operation : A) {
+            /*
+             * Counters defined from input start at 1, the counters array starts
+             * at 0. Decrement operation to get counter index.
+             */
+            int counter = operation - 1;
+            
+            if(operation > N) {
+                startCount = maxCount;
+            } else if(counters[counter] < startCount) {
+                counters[counter] = startCount + 1;
+            } else {
+                counters[counter]++;
+            }
+            
+            /*
+             * If the operation is within the range of counters and the current
+             * counter's value is greater than the current maximum, set the current
+             * maximum to the counter's value.
+             */
+            if(operation <= N && counters[counter] > maxCount) {
+                maxCount = counters[counter];
+            }
+        }
+        
+        /*
+         * Some of the counters may be behind the current startCount, check the
+         * array for these counters and increase them to the startCount.
+         */
+        for(int counter = 0; counter < counters.length; counter++) {
+            if(counters[counter] < startCount) {
+                counters[counter] = startCount;
+            }
+        }
+        
+        /*
+         * Return the counters array.
+         */
+        return counters;
+    }
+}
