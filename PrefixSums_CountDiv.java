6fa64002803ca2e41560d2fff49c05f65f028682
Ali Hooman <me@alihooman.com>
2020-04-10 21:04:23 -0600

    PrefixSums: GenomicRangeQuery

--- PrefixSums_CountDiv.java
+++ PrefixSums_CountDiv.java
@@ -25,6 +25,10 @@
 // you can write to stdout for debugging purposes, e.g.
 // System.out.println("this is a debug message");
 
+/* 
+ * Score: 100%
+ */
+
 class Solution {
     public int solution(int A, int B, int K) {
         /*
